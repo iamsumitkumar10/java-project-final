54a797ad8b4a446f2d00073ff23c38e1d32da528
sumit <sumitku3500@gmail.com>
2025-06-13 18:29:11 +0530

    change port

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
     const employee = { name, designation };
 
     try {
-      const response = await fetch('http://localhost:8080/api/employees', {
+      const response = await fetch('http://localhost:9090/api/employees', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
